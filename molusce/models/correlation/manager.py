4d614ce3aa5d01494edfb084743e76443f0e0bab
Lenar <lenar.khisamiev@yandex.ru>
2013-01-14 20:56:06 +0400

    small modification in programm structure

--- molusce/models/correlation/manager.py
+++ molusce/models/correlation/manager.py
@@ -3,7 +3,8 @@
 import numpy as np
 
 from molusce.dataprovider import Raster
-from model import *
+from molusce.models.correlation.model import correlation, cramer, jiu
+
 class CoeffManagerError(Exception):
     '''Base class for exceptions in this module.'''
     def __init__(self, msg):
@@ -16,7 +17,7 @@ class CoeffManager(object):
     def __init__(self, raster1, raster2):
         '''
         @param raster1    First raster, which must have one bands
-        @param raster2    Second raster, which may have few baands
+        @param raster2    Second raster, which must have one bands
         '''
         self.X = raster1
         self.Y = raster2 
@@ -24,14 +25,12 @@ class CoeffManager(object):
         
         if self.X.getBandsCount() != 1 or self.Y.getBandsCount() != 1:
             raise CoeffManagerError('The rasters must have one band!')
-        description = {'name': self.Y.getFileName()}
-        self.Y = self.Y.getBand(0)
-        self.X = self.X.getBand(0)
-        self.corr   = correlation(self.X, self.Y)
-        self.cramer = cramer(self.X, self.Y)
-        self.joint  = jiu(self.X, self.Y)
-        description['second_raster'] = (self.corr, self.cramer, self.joint)
-        self.coeff.append(description)
+        self.coeff.append(self.Y.getFileName())
+        self.Y = self.Y.getBand(1)
+        self.X = self.X.getBand(1)
+        self.coeff.append(correlation(self.X, self.Y))
+        self.coeff.append(cramer(self.X, self.Y))
+        self.coeff.append(jiu(self.X, self.Y))
         
     def getCoeff(self):
         return self.coeff
