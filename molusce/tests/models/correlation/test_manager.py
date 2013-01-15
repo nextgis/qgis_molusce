7b8444d43d786efd6817e2807683caeda321ed20
Lenar <lenar.khisamiev@yandex.ru>
2013-01-15 21:16:24 +0400

    Use new function getCorr, getCramer, getJIU

--- molusce/tests/models/correlation/test_manager.py
+++ molusce/tests/models/correlation/test_manager.py
@@ -10,7 +10,7 @@ from numpy import ma as ma
 
 from molusce.dataprovider import Raster
 from molusce.models.correlation.manager import CoeffManager
-from molusce.models.correlation.model import 
+from molusce.models.correlation.model import correlation, cramer, jiu
 
 class TestWoEManager (unittest.TestCase):
     def setUp(self):
@@ -18,18 +18,15 @@ class TestWoEManager (unittest.TestCase):
         self.Y = Raster('../../examples/sites.tif')
         
     def test_CoeffManager(self):
-        coeff = CoeffManager(self.X, self.Y).getCoeff()
-        self.assertEqual(len(coeff), 4)
+        coeff = CoeffManager(self.X, self.Y)
+        name = coeff.getName()
         true_name = self.Y.getFileName()
         self.Y = self.Y.getBand(1)
         self.X = self.X.getBand(1)
-        name  = coeff[0]
-        true_coeff = []
-        true_coeff.append(correlation(self.X, self.Y))
-        true_coeff.append(cramer(self.X, self.Y))
-        true_coeff.append(jiu(self.X, self.Y))
         self.assertEqual(name, true_name)        
-        self.assertEqual(true_coeff, coeff[1:])
+        self.assertEqual(coeff.getCorr(), correlation(self.X, self.Y))
+        self.assertEqual(coeff.getCramer(), cramer(self.X, self.Y))
+        self.assertEqual(coeff.getJIU(), jiu(self.X, self.Y))
 
     
 if __name__ == "__main__":
