a7f74416a782937701834c489dd46caed4db0748
Dmitry Kolesov <kolesov.dm@gmail.com>
2013-02-06 14:39:53 +0400

    Split input raster list in Sampler into tuple (current state raster, list of factor rasters)

--- molusce/models/sampler/sampler.py
+++ molusce/models/sampler/sampler.py
@@ -13,32 +13,45 @@ class SamplerError(Exception):
 
 class Sampler(object):
     '''Create training set based on input-output rasters'''
-    def __init__(self, inputs, output=None, ns=0):
+    def __init__(self, state, factors, output=None, ns=0):
         '''
-        @param inputs           List of the input rasters.
-        @param output           Raster that contains classes to predict.
+        @param state            Raster of the current state (classes) values.
+        @param factors          List of the factor rasters (predicting variables).
+        @param output           Raster that contains states (classes) to predict.
         @param ns               Neighbourhood size.
         '''
         
         self.data = None        # Training data
         self.ns = ns
         
-        self.inputVectLen = 0   # Length of input vector
-        for raster in inputs:
-            self.inputVectLen = self.inputVectLen + raster.getNeighbourhoodSize(self.ns)
+        self.outputVecLen = 1                                   # Len of output vector
+        self.stateVecLen  = state.getNeighbourhoodSize(self.ns) # Len of the vector of input states
+        self.factorVectLen = 0                                  # Length of factor vector
+        for raster in factors:
+            self.factorVectLen = self.factorVectLen + raster.getNeighbourhoodSize(self.ns)
     
-    def get_input(self, inputs, row, col):
+    
+    def get_factors(self, factors, row, col):
         '''
         Get input sample at (row, col) pixel and return it as array. Return None if the sample is incomplete.
         '''
-        sample = np.zeros(self.inputVectLen)
-        for (k,raster) in enumerate(inputs):
+        sample = np.zeros(self.factorVectLen)
+        for (k,raster) in enumerate(factors):
             neighbours = raster.getNeighbours(row,col, self.ns).flatten()
             if any(neighbours.mask): # Eliminate incomplete samples
                 return None
             pixel_count = raster.getNeighbourhoodSize(self.ns)
             sample[k*pixel_count: (k+1)*pixel_count] = neighbours
         return sample
+    
+    def get_state(self, state, row, col):
+        '''
+        Get current state at (row, col) pixel and return it as array. Return None if the sample is incomplete.
+        '''
+        neighbours = state.getNeighbours(row,col, self.ns).flatten()
+        if any(neighbours.mask): # Eliminate incomplete samples
+            return None
+        return neighbours
     
     def get_output(self, output, row, col):
         '''
@@ -49,43 +62,46 @@ class Sampler(object):
             return None
         return out
         
-    def setTrainingData(self, inputs, output, shuffle=True):
+    def setTrainingData(self, state, factors, output, shuffle=True):
         '''
         @param inputs           List of the input rasters.
         @param output           Raster that contains classes to predict.
         @param ns               Neighbourhood size.
         @param shuffle          Perform random shuffle.
         '''
-        for r in inputs:
+        for r in factors+[state]:
             if not output.geoDataMatch(r):
                 raise SamplerError('Geometries of the inputs and output rasters are different!')
         
-        #input_vect_len = self.inputVectLen
-        #output_vect_len = 1
         
         (rows,cols) = (output.getXSize(), output.getYSize())
         
         # i,j  are pixel indexes
         for i in xrange(self.ns, rows - self.ns):         # Eliminate the raster boundary (of (ns)-size width) because
             for j in xrange(self.ns, cols - self.ns):     # the samples are incomplete in that region
-                sample = {}
+                sample = np.zeros(1, dtype=[('state', float, self.stateVecLen),('factors',  float, self.factorVectLen), ('output', float, self.outputVecLen)])
                 try: 
                     out = output.getNeighbours(i,j,0).flatten() # Get the pixel
-                    if out == None:                             # Eliminate masked samples
-                        #sample_complete = False
+                    if out == None:                            # Eliminate masked samples
                         continue
                     else:
-                        sample['output'] = out                    
-                    neighbours = self.get_input(inputs, i,j)
+                        sample['output'] = out           
+                        
+                    neighbours = self.get_state(state, i,j)
                     if neighbours == None: # Eliminate incomplete samples
                         continue
-                    sample['input'] = neighbours
+                    sample['state'] = neighbours
+                    
+                    neighbours = self.get_factors(factors, i,j)
+                    if neighbours == None: # Eliminate incomplete samples
+                        continue
+                    sample['factors'] = neighbours
                 except ProviderError:
                     continue
-                try:
-                    self.data.append(sample)
-                except AttributeError:      # This is the first sample
-                    self.data = [sample]
+                if self.data !=None:
+                    self.data = np.hstack( (self.data,sample) )
+                else:      # This is the first sample
+                    self.data = sample
         if shuffle: 
             np.random.shuffle(self.data)
 
--- molusce/tests/models/sampler/test_sampler.py
+++ molusce/tests/models/sampler/test_sampler.py
@@ -14,56 +14,70 @@ from molusce.models.sampler.sampler import Sampler
 
 class TestSample (unittest.TestCase):
     def setUp(self):
-        self.inputs = [Raster('../../examples/multifact.tif')]
+        self.factors = [Raster('../../examples/multifact.tif')]
         self.output = Raster('../../examples/sites.tif')
         
-        self.inputs2 = [Raster('../../examples/multifact.tif'), Raster('../../examples/multifact.tif')]
-        self.inputs3 = [Raster('../../examples/two_band.tif')]
+        self.factors2 = [Raster('../../examples/multifact.tif'), Raster('../../examples/multifact.tif')]
+        self.factors3 = [Raster('../../examples/two_band.tif')]
         
     def test_setTrainingData(self):
-        smp = Sampler(self.inputs, self.output, ns=0)
-        smp.setTrainingData(self.inputs, self.output, shuffle=False)
+        smp = Sampler(self.output, self.factors,  self.output, ns=0)
+        smp.setTrainingData(self.output, self.factors, self.output, shuffle=False)
         
-        data = [
-            {'input': np.array([ 1.]), 'output': np.array([1])}, 
-            {'input': np.array([ 1.]), 'output': np.array([2])}, 
-            {'input': np.array([ 3.]), 'output': np.array([1])}, 
-            {'input': np.array([ 3.]), 'output': np.array([1])}, 
-            {'input': np.array([ 2.]), 'output': np.array([2])}, 
-            {'input': np.array([ 1.]), 'output': np.array([1])}, 
-            {'input': np.array([ 0.]), 'output': np.array([0])}, 
-            {'input': np.array([ 3.]), 'output': np.array([1])}, 
-            {'input': np.array([ 1.]), 'output': np.array([2])}
-        ]
+        data = np.array(
+            [
+                (1.0, 1.0, 1.0),
+                (2.0, 1.0, 2.0),
+                (1.0, 3.0, 1.0),
+                (1.0, 3.0, 1.0),
+                (2.0, 2.0, 2.0),
+                (1.0, 1.0, 1.0),
+                (0.0, 0.0, 0.0),
+                (1.0, 3.0, 1.0),
+                (2.0, 1.0, 2.0),
+            ], 
+            dtype=[('state', float, (1,)), ('factors', float, (1,)), ('output', float, 1)]
+        )
+        
         for i in range(len(data)):
-            assert_array_equal(data[i]['input'], smp.data[i]['input'])
+            assert_array_equal(data[i]['factors'], smp.data[i]['factors'])
             assert_array_equal(data[i]['output'], smp.data[i]['output'])
+            assert_array_equal(data[i]['state'],  smp.data[i]['state'])
 
-        # two input rasters
-        smp = Sampler(self.inputs2, self.output, ns=1)
-        smp.setTrainingData(self.inputs2, self.output)
-        data = [
-            {
-            'input': np.array([ 1.,  1.,  3.,  3.,  2.,  1.,  0.,  3.,  1.,  
-                                1.,  1.,  3.,  3.,  2.,  1.,  0.,  3.,  1.]), 
-            'output': np.array([2])
-            }
-        ]
-        assert_array_equal(data[0]['input'], smp.data[0]['input'])
-        assert_array_equal(data[0]['output'], smp.data[0]['output'])
+        # two factor rasters
+        smp = Sampler(self.output, self.factors2, self.output, ns=1)
+        smp.setTrainingData(self.output, self.factors2, self.output)
         
-        # Multiband input
-        smp = Sampler(self.inputs3, self.output, ns=1)
-        smp.setTrainingData(self.inputs3, self.output)
-        data = [
-            {
-            'input': np.array([ 1.,  2.,  1.,  1.,  2.,  1.,  0.,  1.,  2.,  
-                                1.,  1.,  3.,  3.,  2.,  1.,  0.,  3.,  1.]), 
-            'output': np.array([2])
-            }
-        ]
-        assert_array_equal(data[0]['input'], smp.data[0]['input'])
+        data = np.array(
+            [
+                ([1,2,1,   1,2,1,  0,1,2], 
+                 [ 1.,  1.,  3.,  3.,  2.,  1.,  0.,  3.,  1., 
+                            1.,  1.,  3.,  3.,  2.,  1.,  0.,  3.,  1.], 
+                 2.0)
+            ],
+            dtype=[('state', float, (9,)), ('factors', float, (18,)), ('output', float, 1)]
+        )
+        assert_array_equal(data[0]['factors'], smp.data[0]['factors'])
         assert_array_equal(data[0]['output'], smp.data[0]['output'])
+        assert_array_equal(data[0]['state'],  smp.data[0]['state'])
+        
+        # Multiband factors
+        smp = Sampler(self.output, self.factors3, self.output, ns=1)
+        smp.setTrainingData(self.output, self.factors3, self.output)
+        
+        data = np.array(
+            [
+                ([1,2,1,   1,2,1,  0,1,2], 
+                 [ 1.,  2.,  1.,  1.,  2.,  1.,  0.,  1.,  2.,  
+                                1.,  1.,  3.,  3.,  2.,  1.,  0.,  3.,  1.], 
+                 2.0)
+            ],
+            dtype=[('state', float, (9,)), ('factors', float, (18,)), ('output', float, 1)]
+        )
+        
+        assert_array_equal(data[0]['factors'], smp.data[0]['factors'])
+        assert_array_equal(data[0]['output'], smp.data[0]['output'])
+        assert_array_equal(data[0]['state'],  smp.data[0]['state'])
     
 if __name__ == "__main__":
     unittest.main()
