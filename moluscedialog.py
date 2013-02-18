a6b8387325e0787ed41008c9b75d5e8e419c6034
Alexander Bruy <alexander.bruy@gmail.com>
2013-02-18 17:30:51 +0400

    load layers, ability to select initial, final and factors rasters

--- moluscedialog.py
+++ moluscedialog.py
@@ -26,7 +26,6 @@
 #******************************************************************************
 
 import locale
-import math
 import operator
 
 from PyQt4.QtCore import *
@@ -36,9 +35,67 @@ from qgis.core import *
 
 from ui_moluscedialogbase import Ui_Dialog
 
+import molusceutils as utils
+
 class MolusceDialog(QDialog, Ui_Dialog):
   def __init__(self, iface):
     QDialog.__init__(self)
     self.setupUi(self)
 
     self.iface = iface
+
+    # connect signals and slots
+    self.btnSetInitialRaster.clicked.connect(self.setInitialRaster)
+    self.btnSetFinalRaster.clicked.connect(self.setFinalRaster)
+    self.btnAddFactor.clicked.connect(self.addFactor)
+    self.btnRemoveFactor.clicked.connect(self.removeFactor)
+    self.btnRemoveAllFactors.clicked.connect(self.removeAllFactors)
+
+    self.manageGui()
+
+  def manageGui(self):
+    self.tabWidget.setCurrentIndex(0)
+
+    self.__populateLayers()
+
+    # TODO: restore settings
+
+  def setInitialRaster(self):
+    layerName = self.lstLayers.selectedItems()[0].text()
+    self.leInitRasterName.setText(layerName)
+    self.leInitYear.setText("init year")
+
+  def setFinalRaster(self):
+    layerName = self.lstLayers.selectedItems()[0].text()
+    self.leFinalRasterName.setText(layerName)
+    self.leFinalYear.setText("final year")
+
+  def addFactor(self):
+    layerName = self.lstLayers.selectedItems()[0].text()
+    if len(self.lstFactors.findItems(layerName, Qt.MatchExactly)) > 0:
+      return
+
+    self.lstFactors.insertItem(self.lstFactors.count() + 1, layerName)
+
+  def removeFactor(self):
+    self.lstFactors.takeItem(self.lstFactors.currentRow())
+
+  def removeAllFactors(self):
+    self.lstFactors.clear()
+
+# ******************************************************************************
+
+  def __populateLayers(self):
+    layers = utils.getRasterLayers()
+    relations = self.iface.legendInterface().groupLayerRelationship()
+    for layer in sorted(layers.iteritems(), cmp=locale.strcoll, key=operator.itemgetter(1)):
+      groupName = utils.getLayerGroup(relations, layer[0])
+      item = QListWidgetItem()
+      if groupName == "":
+        item.setText(layer[1])
+        item.setData(Qt.UserRole, layer[0])
+      else:
+        item.setText(QString("%1 - %2").arg(layer[1]).arg(groupName))
+        item.setData(Qt.UserRole, layer[0])
+
+      self.lstLayers.addItem(item)
