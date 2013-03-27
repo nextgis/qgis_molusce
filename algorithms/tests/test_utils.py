99c5130da43f91c01ec76f94640e65fc479de821
Dmitry Kolesov <kolesov.dm@gmail.com>
2013-03-27 11:54:53 +0400

    Fix typos

--- algorithms/tests/test_utils.py
+++ algorithms/tests/test_utils.py
@@ -36,30 +36,30 @@ class TestRaster (unittest.TestCase):
             [False, False, False,],
             [True , False, False,]
         ])
-        
-    def test_masks_identity(self): 
+
+    def test_masks_identity(self):
         self.X, self.Y = masks_identity(self.X, self.Y)
         mask_x = np.matrix.flatten(self.X.mask)
         self.combo_mask = np.matrix.flatten(self.combo_mask)
         k = all(np.equal(mask_x, self.combo_mask))
         self.assertEqual(k, True, 'masks_identify failed')
-    
+
     def test_size_equals(self):
         self.assertEqual(sizes_equal(self.X, self.Y), True, 'incorrent size')
-        
+
     def test_Size_no_equals(self):
         self.assertEqual(sizes_equal(self.X2, self.Y), False, 'sizes are equal')
-        
+
     def test_reclass(self):
         X = reclass(self.X2, [1.1, 3.1, 4])
         answer = np.array([
             [1, 2, 1,],
             [1, 2, 1,],
-            [1, 1, 3,],
+            [1, 1, 4,],
             [1, 1, 4,]
         ])
         assert_array_equal(X, answer)
-    
+
 if __name__ == "__main__":
     unittest.main()
 
