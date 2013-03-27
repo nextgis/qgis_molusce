45f3549e3d384decdbcae3122cdb8ff3b4f6d105
Dmitry Kolesov <kolesov.dm@gmail.com>
2013-03-27 15:28:36 +0400

    MCE model

--- algorithms/models/mce/mce.py
+++ algorithms/models/mce/mce.py
@@ -7,6 +7,7 @@
 import numpy as np
 
 from molusce.algorithms.dataprovider import Raster, ProviderError
+from molusce.algorithms.utils import binaryzation
 
 class MCEError(Exception):
     '''Base class for exceptions in this module.'''
@@ -56,24 +57,24 @@ class MCE(object):
         38: 1.70,
         39: 1.70
     }
-    def __init__(self, factors, wMatr, areaAnalyst, initStateNum, finalStateNum):
+    def __init__(self, factors, wMatr, initStateNum, finalStateNum):
         '''
         Multicriteria evaluation based on Saaty method. It defines transition probability of two classes (initStateNum, finalStateNum).
         @param factors          List of the factor rasters used for prediction.
         @param wMatr            List of lists -- NxN comparison matrix.
-        @param areaAnalyst      AreaAnalyst that contains map of the changes, encodes and decodes class numbers.
         @param initStateNum     Number of initial state (the state before transition).
         @param finalStateNum    Number of final state (the state after transition).
         '''
 
         self.factors = factors
-        self.analyst = areaAnalyst
-        self.changeMap   = areaAnalyst.getChangeMap()
+        self.initStateNum  = initStateNum
+        self.finalStateNum = finalStateNum
 
+        # Check matrix dimension and factor count, apply normalization
         self.dim = 0
         for f in factors:
             self.dim = self.dim + f.getBandsCount()
-
+            f.normalize(mode = 'maxmin')
         if self.dim != len(wMatr):
             raise MCEError('Matrix size is different from the number of variables!')
 
@@ -83,13 +84,19 @@ class MCE(object):
                 raise MCEError('The weight matrix is not NxN!')
         EPSILON = 0.000001      # A small number
         for i in xrange(self.dim):
+            if wMatr[i][i] != 1:
+                raise MCEError('w[i,i] not equal 1 !')
             for j in xrange(i+1, self.dim):
                 if abs(wMatr[i][j] * wMatr[j][i] - 1) > EPSILON:
                     raise MCEError('w[i,j] * w[j,i] not equal 1 !')
 
         self.wMatr = np.array(wMatr)
 
-        self.weights = None     # Weigths of the factors, calculated using wMatr
+        self.weights = None     # Weights of the factors, calculated using wMatr
+                                # It's a list, the length is self.dim
+                                # first element is the weight of first band of the first factor and so on:
+                                # [W_f1, ... weights of 1-st factors ... , W_f2, ... weights of 1-st factors..., W_fn, ...]
+
         self.consistency =None  # Consistency ratio of the comparison matrix.
 
         self.prediction = None
@@ -117,7 +124,47 @@ class MCE(object):
         return self.weights
 
     def _predict(self, state):
-        pass
+        '''
+        Predict the changes.
+        '''
+        geodata = state.getGeodata()
+        rows, cols = geodata['ySize'], geodata['xSize']
+
+        # Get locations where self.initStateNum is occurs
+        band = state.getBand(1)
+        initStateMask = binaryzation(band, [self.initStateNum])
+        mask = band.mask
+
+        # Calculate summary map of factors weights
+        # Confidence:
+        #   confidence is summary map of factors, if current state = self.initState
+        #   confidence is 0, if current state != self.initState
+        # Prediction:
+        #   predicted value is a constant = self.finalStateNum, if current state = self.initState
+        #   predicted value is current state, if current state != self.initState
+        confidence = np.zeros((rows,cols))
+        weights = self.getWeights()
+        weightNum = 0               # Number of processed weights
+        for f in self.factors:
+            if not f.geoDataMatch(state):
+                raise MCEError('Geometries of the state and factor rasters are different!')
+            f.normalize(mode = 'maxmin')
+            for i in xrange(f.getBandsCount()):
+                band = f.getBand(i+1)
+                confidence = confidence + band*weights[weightNum]
+                mask = np.ma.mask_or(mask, band.mask)
+                weightNum = weightNum + 1
+        confidence = confidence*initStateMask
+        prediction = np.copy(state.getBand(1))
+        prediction = np.logical_not(initStateMask) * prediction
+        prediction = prediction + initStateMask*self.finalStateNum
+
+        predicted_band = np.ma.array(data=prediction, mask=mask)
+        self.prediction = Raster()
+        self.prediction.create([predicted_band], geodata)
+        confidence_band = np.ma.array(data=confidence, mask=mask)
+        self.confidence = Raster()
+        self.confidence.create([confidence_band], geodata)
 
     def setWeights(self):
         '''
--- algorithms/tests/models/mce/test_mce.py
+++ algorithms/tests/models/mce/test_mce.py
@@ -6,7 +6,7 @@ sys.path.insert(0, '../../../../../')
 import unittest
 
 import numpy as np
-from numpy.testing import assert_allclose
+from numpy.testing import assert_almost_equal
 
 from molusce.algorithms.dataprovider import Raster
 from molusce.algorithms.models.area_analysis.manager import AreaAnalyst
@@ -22,21 +22,51 @@ class TestMCE(unittest.TestCase):
 
         self.state  = Raster('../../examples/sites.tif')
         self.state.resetMask(maskVals= [0])
+            #~ [1,2,1],
+            #~ [1,2,1],
+            #~ [0,1,2]
     def test_MCE(self):
 
-        aa = AreaAnalyst(self.state, self.state)
         data = [
             [1.0,     4.0, 6.0, 7.0],
             [1.0/4,   1.0, 3.0, 4.0],
             [1.0/6, 1.0/3, 1.0, 2.0],
             [1.0/7, 1.0/4, 1.0/2, 1]
         ]
-        mce = MCE([self.factor, self.factor, self.factor, self.factor], data, aa, 1, 2)
+        # Multiband
+        factor = Raster('../../examples/two_band.tif')
+        mce = MCE([self.factor, factor, self.factor], data, 1, 2)
         w = mce.getWeights()
         answer = [0.61682294, 0.22382863, 0.09723423, 0.06211421]
-        assert_allclose(w, answer)
+        assert_almost_equal(w, answer)
 
+        # One-band
+        mce = MCE([self.factor, self.factor, self.factor, self.factor], data, 1, 2)
+        w = mce.getWeights()
+        answer = [0.61682294, 0.22382863, 0.09723423, 0.06211421]
+        assert_almost_equal(w, answer)
 
+        mask = [
+            [False, False, False],
+            [False, False, False],
+            [False, False, True]
+        ]
+        p = mce.getPrediction(self.state).getBand(1)
+        answer = [      # The locations where the big numbers are stored must be masked (see mask and self.state)
+            [2, 2, 2],
+            [2, 2, 2],
+            [100, 2, 100]
+        ]
+        answer = np.ma.array(data = answer, mask = mask)
+        assert_almost_equal(p, answer)
+        c = mce.getConfidence().getBand(1)
+        answer = [      # The locations where the big numbers are stored must be masked (see mask and self.state)
+            [1.0/3,  0,  1  ],
+            [1,     0,  1.0/3],
+            [1000,  1,  1000]
+        ]
+        answer = np.ma.array(data = answer, mask = mask)
+        assert_almost_equal(c, answer)
 
 if __name__ == "__main__":
     unittest.main()
