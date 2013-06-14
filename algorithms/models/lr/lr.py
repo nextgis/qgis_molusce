Add transition potential calculation sceleton to molusce widget; Fix spelling errors

algorithms/models/lr/lr.py
@@ -49,7 +49,7 @@ class LR(QObject):
         self.prediction = None  # Raster of the LR prediction results
         self.confidence = None  # Raster of the LR results confidence (1 = the maximum confidence, 0 = the least confidence)
         self.Kappa      = 0     #  Kappa value
-        self.transitionPotencials = None # Dictionary of transition potencial maps: {category1: map1, category2: map2, ...}
+        self.transitionPotentials = None # Dictionary of transition potencial maps: {category1: map1, category2: map2, ...}
 
     def getCoef(self):
         return self.logreg.get_weights().T
@@ -79,12 +79,12 @@ class LR(QObject):
         X = np.column_stack( (self.data['state'], self.data['factors']) )
         return self.logreg.get_pval_weights(X).T
 
-    def getPrediction(self, state, factors):
+    def getPrediction(self, state, factors, calcTransitions=False):
         self._predict(state, factors)
         return self.prediction
 
-    def getTransitionPotencials(self):
-        return self.transitionPotencials
+    def getTransitionPotentials(self):
+        return self.transitionPotentials
 
     def _outputConfidence(self, input):
         '''
@@ -110,7 +110,7 @@ class LR(QObject):
                 if not state.geoDataMatch(r):
                     raise LRError('Geometries of the input rasters are different!')
 
-            self.transitionPotencials = None    # Reset tr.potencials if they exist
+            self.transitionPotentials = None    # Reset tr.potentials if they exist
 
             # Normalize factors before prediction:
             for f in factors:

algorithms/models/mlp/manager.py
@@ -59,7 +59,7 @@ class MlpManager(QObject):
         # Results of the MLP prediction
         self.prediction = None  # Raster of the MLP prediction results
         self.confidence = None  # Raster of the MLP results confidence (1 = the maximum confidence, 0 = the least confidence)
-        self.transitionPotencials = None # Dictionary of transition potencial maps: {category1: map1, category2: map2, ...}
+        self.transitionPotentials = None # Dictionary of transition potencial maps: {category1: map1, category2: map2, ...}
 
         # Outputs of the activation function for small and big numbers
         self.sigmax, self.sigmin = sigmoid(100), sigmoid(-100)  # Max and Min of the sigmoid function
@@ -180,8 +180,8 @@ class MlpManager(QObject):
     def getTrainError(self):
         return self.train_error
 
-    def getTransitionPotencials(self):
-        return self.transitionPotencials
+    def getTransitionPotentials(self):
+        return self.transitionPotentials
 
     def getValError(self):
         return self.val_error
@@ -231,7 +231,7 @@ class MlpManager(QObject):
             if not state.geoDataMatch(r):
                 raise MlpManagerError('Geometries of the input rasters are different!')
 
-        self.transitionPotencials = None    # Reset tr.potencials if they exist
+        self.transitionPotentials = None    # Reset tr.potentials if they exist
 
         # Normalize factors before prediction:
         for f in factors:
@@ -240,9 +240,9 @@ class MlpManager(QObject):
         predicted_band  = np.zeros([rows, cols])
         confidence_band = np.zeros([rows, cols])
         if calcTransitions:
-            self.transitionPotencials = {}
+            self.transitionPotentials = {}
             for cat in self.catlist:
-                self.transitionPotencials[cat] = np.zeros([rows, cols])
+                self.transitionPotentials[cat] = np.zeros([rows, cols])
 
         self.sampler = Sampler(state, factors, ns=self.ns)
         mask = state.getBand(1).mask.copy()
@@ -263,10 +263,10 @@ class MlpManager(QObject):
                         confidence_band[i, j] = confidence
 
                         if calcTransitions:
-                            potencials = self.outputTransitions(out)
+                            potentials = self.outputTransitions(out)
                             for cat in self.catlist:
-                                map = self.transitionPotencials[cat]
-                                map[i, j] = potencials[cat]
+                                map = self.transitionPotentials[cat]
+                                map[i, j] = potentials[cat]
                     else: # Input sample is incomplete => mask this pixel
                         mask[i, j] = True
             self.updateProgress.emit()
@@ -280,9 +280,9 @@ class MlpManager(QObject):
 
         if calcTransitions:
             for cat in self.catlist:
-                band = [np.ma.array(data=self.transitionPotencials[cat], mask=mask)]
-                self.transitionPotencials[cat] = Raster()
-                self.transitionPotencials[cat].create(band, geodata)
+                band = [np.ma.array(data=self.transitionPotentials[cat], mask=mask)]
+                self.transitionPotentials[cat] = Raster()
+                self.transitionPotentials[cat].create(band, geodata)
 
     def readMlp(self):
         pass

algorithms/models/woe/manager.py
@@ -80,7 +80,7 @@ class WoeManager(QObject):
         #   ...
         #}
         #
-        self.transitionPotencials = None # Dictionary of transition potencial maps: {category1: map1, category2: map2, ...}
+        self.transitionPotentials = None # Dictionary of transition potencial maps: {category1: map1, category2: map2, ...}
 
 
     def checkBins(self):
@@ -108,15 +108,15 @@ class WoeManager(QObject):
     def getConfidence(self):
         return self.confidence
 
-    def getPrediction(self, state, factors=None):
+    def getPrediction(self, state, factors=None, calcTransitions=False):
         '''
         Most of the models use factors for prediction, but WoE takes list of factors only once (during the initialization).
         '''
-        self._predict(state)
+        self._predict(state, calcTransitions)
         return self.prediction
 
-    def getTransitionPotencials(self):
-        return self.transitionPotencials
+    def getTransitionPotentials(self):
+        return self.transitionPotentials
 
     def getWoe(self):
         return self.woe
@@ -132,7 +132,7 @@ class WoeManager(QObject):
             if not self.changeMap.geoDataMatch(state):
                 raise WoeManagerError('Geometries of the state and changeMap rasters are different!')
 
-            self.transitionPotencials = None    # Reset tr.potencials if they exist
+            self.transitionPotentials = None    # Reset tr.potentials if they exist
 
             prediction = np.zeros((rows,cols))
             confidence = np.zeros((rows,cols))

algorithms/tests/models/mlp/test_manager.py
@@ -172,11 +172,11 @@ class TestMlpManager (unittest.TestCase):
         # Confidence is zero
         confid = mng.getConfidence()
         assert_array_equal(confid.getBand(1), np.zeros((3,3)))
-        # Transition Potencials (is (sigmoid(0) - sigmin)/sigrange )
-        potencials = mng.getTransitionPotencials()
+        # Transition Potentials (is (sigmoid(0) - sigmin)/sigrange )
+        potentials = mng.getTransitionPotentials()
         cats = self.output.getBandGradation(1)
         for cat in cats:
-            map = potencials[cat]
+            map = potentials[cat]
             assert_array_equal(map.getBand(1), (sigmoid(0) - mng.sigmin)*np.ones((3,3))/mng.sigrange )
 
     # Commented while we don't have free rasters to test