55819c3ddae996a0bd4fa0b8ebcdf78ac89d967a
Dmitry Kolesov <kolesov.dm@gmail.com>
2013-06-14 18:41:05 +0400

    Use try-except statement in prediction to prevent silent crash

--- algorithms/models/mlp/manager.py
+++ algorithms/models/mlp/manager.py
@@ -224,65 +224,68 @@ class MlpManager(QObject):
         @param state            Raster of the current state (categories) values.
         @param factors          List of the factor rasters (predicting variables).
         '''
-        self.rangeChanged.emit(self.tr("Initialize model %p%"), 1)
-        geodata = state.getGeodata()
-        rows, cols = geodata['ySize'], geodata['xSize']
-        for r in factors:
-            if not state.geoDataMatch(r):
-                raise MlpManagerError('Geometries of the input rasters are different!')
+        try:
+            self.rangeChanged.emit(self.tr("Initialize model %p%"), 1)
+            geodata = state.getGeodata()
+            rows, cols = geodata['ySize'], geodata['xSize']
+            for r in factors:
+                if not state.geoDataMatch(r):
+                    raise MlpManagerError('Geometries of the input rasters are different!')
 
-        self.transitionPotentials = None    # Reset tr.potentials if they exist
+            self.transitionPotentials = None    # Reset tr.potentials if they exist
 
-        # Normalize factors before prediction:
-        for f in factors:
-            f.normalize(mode = 'mean')
+            # Normalize factors before prediction:
+            for f in factors:
+                f.normalize(mode = 'mean')
 
-        predicted_band  = np.zeros([rows, cols])
-        confidence_band = np.zeros([rows, cols])
-        if calcTransitions:
-            self.transitionPotentials = {}
-            for cat in self.catlist:
-                self.transitionPotentials[cat] = np.zeros([rows, cols])
+            predicted_band  = np.zeros([rows, cols])
+            confidence_band = np.zeros([rows, cols])
+            if calcTransitions:
+                self.transitionPotentials = {}
+                for cat in self.catlist:
+                    self.transitionPotentials[cat] = np.zeros([rows, cols])
 
-        self.sampler = Sampler(state, factors, ns=self.ns)
-        mask = state.getBand(1).mask.copy()
-        if mask.shape == ():
-            mask = np.zeros([rows, cols])
-        self.updateProgress.emit()
-        self.rangeChanged.emit(self.tr("Prediction %p%"), rows)
-        for i in xrange(rows):
-            for j in xrange(cols):
-                if not mask[i,j]:
-                    input = self.sampler.get_inputs(state, i,j)
-                    if input != None:
-                        out = self.getOutput(input)
-                        res = self.outCategory(out)
-                        predicted_band[i, j] = res
-
-                        confidence = self.outputConfidence(out)
-                        confidence_band[i, j] = confidence
-
-                        if calcTransitions:
-                            potentials = self.outputTransitions(out)
-                            for cat in self.catlist:
-                                map = self.transitionPotentials[cat]
-                                map[i, j] = potentials[cat]
-                    else: # Input sample is incomplete => mask this pixel
-                        mask[i, j] = True
+            self.sampler = Sampler(state, factors, ns=self.ns)
+            mask = state.getBand(1).mask.copy()
+            if mask.shape == ():
+                mask = np.zeros([rows, cols])
             self.updateProgress.emit()
-        predicted_bands  = [np.ma.array(data = predicted_band, mask = mask)]
-        confidence_bands = [np.ma.array(data = confidence_band, mask = mask)]
+            self.rangeChanged.emit(self.tr("Prediction %p%"), rows)
+            for i in xrange(rows):
+                for j in xrange(cols):
+                    if not mask[i,j]:
+                        input = self.sampler.get_inputs(state, i,j)
+                        if input != None:
+                            out = self.getOutput(input)
+                            res = self.outCategory(out)
+                            predicted_band[i, j] = res
 
-        self.prediction = Raster()
-        self.prediction.create(predicted_bands, geodata)
-        self.confidence = Raster()
-        self.confidence.create(confidence_bands, geodata)
+                            confidence = self.outputConfidence(out)
+                            confidence_band[i, j] = confidence
 
-        if calcTransitions:
-            for cat in self.catlist:
-                band = [np.ma.array(data=self.transitionPotentials[cat], mask=mask)]
-                self.transitionPotentials[cat] = Raster()
-                self.transitionPotentials[cat].create(band, geodata)
+                            if calcTransitions:
+                                potentials = self.outputTransitions(out)
+                                for cat in self.catlist:
+                                    map = self.transitionPotentials[cat]
+                                    map[i, j] = potentials[cat]
+                        else: # Input sample is incomplete => mask this pixel
+                            mask[i, j] = True
+                self.updateProgress.emit()
+            predicted_bands  = [np.ma.array(data = predicted_band, mask = mask)]
+            confidence_bands = [np.ma.array(data = confidence_band, mask = mask)]
+
+            self.prediction = Raster()
+            self.prediction.create(predicted_bands, geodata)
+            self.confidence = Raster()
+            self.confidence.create(confidence_bands, geodata)
+
+            if calcTransitions:
+                for cat in self.catlist:
+                    band = [np.ma.array(data=self.transitionPotentials[cat], mask=mask)]
+                    self.transitionPotentials[cat] = Raster()
+                    self.transitionPotentials[cat].create(band, geodata)
+        except:
+            self.logMessage.emit(self.tr("An error occurs during ANN prediction"))
 
     def readMlp(self):
         pass
