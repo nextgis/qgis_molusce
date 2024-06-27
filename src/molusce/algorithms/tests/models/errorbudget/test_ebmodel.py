8e1bcf43d7c25ac6d691a6410e12102a4e80da72
Valeriy Remeshevskiy <mr.remeshevskiy@mail.com>
2024-06-27 18:42:27 +0300

    Apply src layout

---
+++ src/molusce/algorithms/tests/models/errorbudget/test_ebmodel.py
@@ -0,0 +1,190 @@
+# encoding: utf-8
+
+import sys
+sys.path.insert(0, '../../../../../')
+
+import unittest
+
+import numpy as np
+from numpy import ma as ma
+
+from molusce.algorithms.dataprovider import Raster
+from molusce.algorithms.models.errorbudget.ebmodel import EBudget, weightedSum
+
+
+class TestModel (unittest.TestCase):
+    def setUp(self):
+        self.reference = Raster('../../examples/data.tif')
+            #~ [1,1,1,1],
+            #~ [1,1,2,2],
+            #~ [2,2,2,2],
+            #~ [3,3,3,3]
+
+        self.simulated = Raster('../../examples/data1.tif')
+            #~ [1,1,2,3],
+            #~ [3,1,2,3],
+            #~ [3,3,3,3],
+            #~ [1,1,3,2]
+
+    def test_Init(self):
+        Rj = {1: np.ma.array(data =
+                 [[1.0, 1.0, 1.0, 1.0],
+                 [1.0, 1.0, 0.0, 0.0],
+                 [0.0, 0.0, 0.0, 0.0],
+                 [0.0, 0.0, 0.0, 0.0]],
+                             mask =
+                 [[False, False, False, False],
+                 [False, False, False, False],
+                 [False, False, False, False],
+                 [False, False, False, False]],
+                ),
+            2: np.ma.array(data =
+                 [[0.0, 0.0, 0.0, 0.0],
+                 [0.0, 0.0, 1.0, 1.0],
+                 [1.0, 1.0, 1.0, 1.0],
+                 [0.0, 0.0, 0.0, 0.0]],
+                             mask =
+                 [[False, False, False, False],
+                 [False, False, False, False],
+                 [False, False, False, False],
+                 [False, False, False, False]],
+                ),
+            3: np.ma.array(data =
+                 [[0.0, 0.0, 0.0, 0.0],
+                 [0.0, 0.0, 0.0, 0.0],
+                 [0.0, 0.0, 0.0, 0.0],
+                 [1.0, 1.0, 1.0, 1.0]],
+                             mask =
+                [[False, False, False, False],
+                 [False, False, False, False],
+                 [False, False, False, False],
+                 [False, False, False, False]],
+                )
+        }
+
+        eb = EBudget(self.reference, self.simulated)
+        np.testing.assert_equal(eb.Rj, Rj)
+
+    def test_weightedSum(self):
+        eb = EBudget(self.reference, self.simulated)
+        W = eb.W
+
+        S1 = weightedSum(eb.Sj[1], W)
+        S2 = weightedSum(eb.Sj[2], W)
+        S3 = weightedSum(eb.Sj[3], W)
+        self.assertEqual(S1, 5.0/16)
+        self.assertEqual(S2, 3.0/16)
+        self.assertEqual(S3, 8.0/16)
+
+
+
+    def test_NoNo(self):
+        eb = EBudget(self.reference, self.simulated)
+
+        noNo = eb.NoNo()
+        answer = 1.0/3
+        np.testing.assert_almost_equal(noNo, answer)
+
+    def test_NoMed(self):
+        eb = EBudget(self.reference, self.simulated)
+
+        noM = eb.NoMed()
+        answer = (6.0*5/16 + 6.0*3/16 + 4.0*8/16)/16
+        np.testing.assert_almost_equal(noM, answer)
+
+    def test_MedMed(self):
+        eb = EBudget(self.reference, self.simulated)
+        medM= eb.MedMed()
+        answer = 5.0/16
+        np.testing.assert_almost_equal(medM, answer)
+
+    def test_MedPer(self):
+        eb = EBudget(self.reference, self.simulated)
+        medP= eb.MedPer()
+        answer = min(6.0/16, 5.0/16) + min(6.0/16, 3.0/16) + min(4.0/16, 8.0/16)
+        np.testing.assert_almost_equal(medP, answer)
+
+    def test_Mask(self):
+        reference = Raster('../../examples/data.tif')
+        simulated = Raster('../../examples/data1.tif')
+        reference.resetMask([2])
+        simulated.resetMask([2])
+
+        eb = EBudget(reference, simulated)
+        W = eb.W
+
+        S1 = weightedSum(eb.Sj[1], W)
+        S3 = weightedSum(eb.Sj[3], W)
+        np.testing.assert_almost_equal(S1, 5.0/8)
+        np.testing.assert_almost_equal(S3, 3.0/8)
+
+        noNo = eb.NoNo()
+        np.testing.assert_almost_equal(noNo, 0.5)
+
+        noM = eb.NoMed()
+        np.testing.assert_almost_equal(noM, (5.0*5/8 + 3.0*3/8)/8)
+
+        medM= eb.MedMed()
+        np.testing.assert_almost_equal(medM, 4.0/8)
+
+        medP= eb.MedPer()
+        np.testing.assert_almost_equal(medP, 1.0)
+
+    def test_coarse(self):
+        reference = Raster('../../examples/data.tif')
+        simulated = Raster('../../examples/data1.tif')
+        reference.resetMask([2])
+        simulated.resetMask([2])
+
+        eb = EBudget(reference, simulated)
+        eb.coarse(2)
+        # W
+        answer = np.array([[1.0, 0.25], [0.5, 0.25]])
+        np.testing.assert_array_equal(eb.W, answer)
+        # Rj
+        answer1 = np.array([[1.0, 1.0], [0, 0]])
+        answer3 = np.array([[0, 0], [1.0, 1.0]])
+        ans = {1: answer1, 3: answer3}
+        np.testing.assert_equal(eb.Rj, ans)
+        # Sj
+        answer1 = np.array([[3.0/4, 0.0], [1.0, 0]])
+        answer3 = np.array([[1.0/4, 1.0], [0, 1.0]])
+        ans = {1: answer1, 3: answer3}
+        np.testing.assert_equal(eb.Sj, ans)
+
+        eb.coarse(2)
+        # W
+        answer = np.array([[0.5]])
+        np.testing.assert_array_equal(eb.W, answer)
+        # Rj
+        answer1 = np.array([[(1+1.0/4)/2]])
+        answer3 = np.array([[(1.0/2 + 1.0/4)/2]])
+        ans = {1: answer1, 3: answer3}
+        np.testing.assert_equal(eb.Rj, ans)
+        # Sj
+        answer1 = np.array([[(3.0/4 + 0.5)/2]])
+        answer3 = np.array([[(1.0/4 + 1.0/4 + 1.0/4 )/2]])
+        ans = {1: answer1, 3: answer3}
+        np.testing.assert_equal(eb.Sj, ans)
+
+    def test_getStat(self):
+        reference = Raster('../../examples/data.tif')
+        simulated = Raster('../../examples/data1.tif')
+        reference.resetMask([2])
+        simulated.resetMask([2])
+
+        eb = EBudget(reference, simulated)
+        stat = eb.getStat(nIter=3)
+        ans0 =  {'NoNo': 0.5, 'NoMed': (5.0*5/8 + 3.0*3/8)/8, 'MedMed': 4.0/8, 'MedPer': 1.0, 'PerPer': 1.0}
+        for k in list(stat[0].keys()):
+            np.testing.assert_almost_equal(stat[0][k],ans0[k])
+        ans1 = {'NoNo': 0.5, 'NoMed': (5.0/8+5.0/32 + 3.0/16 + 3.0/32)/2, 'MedMed': 4.0/8, 'MedPer': 1.0, 'PerPer': 1.0}
+        for k in list(stat[1].keys()):
+            np.testing.assert_almost_equal(stat[0][k],ans1[k])
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/molusce/algorithms/tests/models/sim/test_sim.py
@@ -0,0 +1,131 @@
+# encoding: utf-8
+
+import sys
+sys.path.insert(0, '../../../../../')
+
+import unittest
+from numpy.testing import assert_array_equal
+
+import numpy as np
+from numpy import ma as ma
+
+from molusce.algorithms.models.crosstabs.manager  import CrossTableManager
+from molusce.algorithms.models.area_analysis.manager import AreaAnalyst
+from molusce.algorithms.dataprovider import Raster
+from molusce.algorithms.models.simulator.sim import Simulator
+
+
+class Model(object):
+    '''
+    Simple predicting model for Simulator tests
+    '''
+    def __init__(self, state):
+        self._predict(state)
+
+    def getConfidence(self):
+        return self.confidence
+
+    def getPrediction(self, state, factors=None, calcTransitions=False):
+        self._predict(state, factors)
+        return self.prediction
+
+    def _predict(self, state, factors = None, calcTransitions=False):
+        geodata = state.getGeodata()
+        band = state.getBand(1)
+        rows, cols = geodata['ySize'], geodata['xSize']
+        # Let the new state is: 1 -> 2, 2- >3, 3 -> 1, then
+        # the prediction is 1->1, 2->5, 3->6
+
+        predicted_band  = np.copy(band)
+        predicted_band[band == 1] = 1.0
+        predicted_band[band == 2] = 5.0
+        predicted_band[band == 3] = 6.0
+
+        # Let the confidence is 1/(1+row+col), where row is row number of the cell, col is column number of the cell.
+        confidence_band = np.zeros([rows, cols])
+        for i in range(cols):
+            for j in range(rows):
+                confidence_band[i,j] = 1.0/(1+i+j)
+
+        predicted_bands  = [np.ma.array(data = predicted_band, mask = band.mask)]
+        confidence_bands = [np.ma.array(data = confidence_band, mask = band.mask)]
+        self.prediction = Raster()
+        self.prediction.create(predicted_bands, state.geodata)
+        self.confidence = Raster()
+        self.confidence.create(confidence_bands, state.geodata)
+
+
+
+class TestSimulator(unittest.TestCase):
+
+    def setUp(self):
+
+        # Raster1:
+            #~ [1, 1, 3,],
+            #~ [3, 2, 1,],
+            #~ [0, 3, 1,]
+        self.raster1 = Raster('../../examples/multifact.tif')
+        self.raster1.resetMask([0])
+
+        self.X = np.array([
+            [1, 2, 3],
+            [3, 2, 1],
+            [0, 1, 1]
+        ])
+        self.X = np.ma.array(self.X, mask=(self.X == 0))
+        self.raster2 = Raster()
+        self.raster2.create([self.X], self.raster1.getGeodata())
+
+        self.aa = AreaAnalyst(self.raster1, self.raster2)
+
+        self.crosstab = CrossTableManager(self.raster1, self.raster2)
+
+        # Simple model
+        self.model = Model(state=self.raster1)
+
+    def test_compute_table(self):
+
+        # print self.crosstab.getCrosstable().getCrosstable()
+        # CrossTab:
+        #  [[ 3.  1.  0.]
+        #   [ 0.  1.  0.]
+        #   [ 1.  0.  2.]]
+        prediction = self.model.getPrediction(self.raster1)
+        # print prediction.getBand(1)
+        # prediction = [[1.0 1.0 6.0]
+                     #  [6.0 5.0 1.0]
+                     #  [-- 6.0 1.0]]
+        # confidence = self.model.getConfidence()
+        # print confidence.getBand(1)
+        # confidence =     [[1.0 0.5  0.33]
+                         #  [0.5 0.33 0.25]
+                         #  [--  0.25 0.2]]
+        result = np.array([
+            [2.0, 1.0, 3.0],
+            [1.0, 2.0, 1.0],
+            [0,   3.0, 1.0]
+        ])
+        result = np.ma.array(result, mask = (result==0))
+
+        simulator = Simulator(state=self.raster1, factors=None, model=self.model, crosstable=self.crosstab)    # The model does't use factors
+        simulator.setIterationCount(1)
+        simulator.simN()
+        state = simulator.getState().getBand(1)
+        assert_array_equal(result, state)
+
+        result = np.array([
+            [2.0, 1.0, 1.0],
+            [2.0, 2.0, 1.0],
+            [0,   3.0, 1.0]
+        ])
+        result = np.ma.array(result, mask = (result==0))
+
+        simulator = Simulator(self.raster1, None, self.model, self.crosstab)
+        simulator.setIterationCount(2)
+        simulator.simN()
+        state = simulator.getState().getBand(1)
+        assert_array_equal(result, state)
+
+
+if __name__ == "__main__":
+    unittest.main()
