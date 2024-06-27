64e614f197040b933f92c4f7f3eb22557cb624a9
Valeriy Remeshevskiy <mr.remeshevskiy@mail.com>
2024-06-27 16:04:40 +0300

    Change qt5 import

--- moluscedialog.py
+++ moluscedialog.py
@@ -38,12 +38,12 @@ from qgis.PyQt.QtGui import *
 from qgis.core import *
 
 import numpy
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 try:
-  from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
+  from matplotlib.backends.backend_qt5agg import NavigationToolbar2QTAgg as NavigationToolbar
 except ImportError:
-  from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+  from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
 from matplotlib.figure import Figure
 
--- neuralnetworkwidget.py
+++ neuralnetworkwidget.py
@@ -28,16 +28,18 @@
 from qgis.PyQt.QtCore import *
 from qgis.PyQt.QtGui import *
 
+from qgis.PyQt.QtWidgets import QWidget
+
 from qgis.core import *
 
 import numpy
 
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 try:
-  from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
+  from matplotlib.backends.backend_qt5agg import NavigationToolbar2QTAgg as NavigationToolbar
 except ImportError:
-  from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+  from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
 from matplotlib.figure import Figure
 from matplotlib import rcParams
