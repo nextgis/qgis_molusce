36ee11386d170e10d8722581bcec6f396a8345d7
Valeriy Remeshevskiy <mr.remeshevskiy@mail.ru>
2024-08-27 17:41:35 +0300

    Add a new check to the test_AreaAnalyst

--- tests/models/area_analysis/test_manager.py
+++ tests/models/area_analysis/test_manager.py
@@ -9,17 +9,21 @@ from numpy.testing import assert_array_equal
 
 class TestAreaAnalysisManager(unittest.TestCase):
     def setUp(self):
-        sample_path = Path(__file__).parents[2] / "examples" / "multifact.tif"
-        self.r1 = Raster(sample_path)
+        examples_path = Path(__file__).parents[2] / "examples"
+        self.r1 = Raster(examples_path / "multifact.tif")
         # r1 -> r1 transition
         self.r1r1 = [[5, 5, 15], [15, 10, 5], [0, 15, 5]]
 
-        self.r2 = Raster(sample_path)
+        self.r2 = Raster(examples_path / "multifact.tif")
         self.r2.resetMask([0])
         self.r2r2 = [[0, 0, 8], [8, 4, 0], [100, 8, 0]]
 
-        self.r3 = Raster(sample_path)
+        self.r3 = Raster(examples_path / "multifact.tif")
         self.r3.resetMask([2])
+
+        # Rasters with an uneven number of classes
+        self.r4 = Raster(examples_path / "raster_n_classes.tif")
+        self.r5 = Raster(examples_path / "raster_n_plus_1_classes.tif")
 
     def test_AreaAnalyst(self):
         aa = AreaAnalyst(self.r1, self.r1)
@@ -32,6 +36,12 @@ class TestAreaAnalysisManager(unittest.TestCase):
         raster = aa.getChangeMap()
         band = raster.getBand(1)
         assert_array_equal(band, self.r2r2)
+
+        # Checking the equalization of class lists
+        area_analyst = AreaAnalyst(self.r4, self.r5)
+        self.assertEqual(
+            area_analyst.categories, area_analyst.categoriesSecond
+        )
 
     def test_encode(self):
         aa = AreaAnalyst(self.r1, self.r1)
