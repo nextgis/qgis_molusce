0755746c805664aeb56a18a94ed12ff6211b22d9
Ivan Barsukov <i.barsukov@protonmail.com>
2024-09-04 20:23:16 +0700

    Remove unneeded files

--- update_translations.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PACKAGE="molusce"
-SRC_FILES=`find . ! -path "src/$PACKAGE/*" \( -name "*.ui" -o -name "*.py" \) | tr "\n" " "`
-TS_FILE=src/$PACKAGE/i18n/*.ts
-pylupdate5 -noobsolete $SRC_FILES -ts $TS_FILE
