3cac1a4f943044830bf05e304267a9d08fc6e60a
ee.kazakov <ee.kazakov@gmail.com>
2025-03-13 17:17:01 +0100

    implementation of separate factors version support

--- src/molusce/algorithms/models/simulator/sim.py
+++ src/molusce/algorithms/models/simulator/sim.py
@@ -10,7 +10,7 @@ class Simulator(QObject):
     over a number of cycles
     """
 
-    rangeChanged = pyqtSignal(str, int)
+    rangeChanged = pyqtSignal(str, int) 
     updateProgress = pyqtSignal()
     simFinished = pyqtSignal()
     logMessage = pyqtSignal(str)
